56f23926736ac0dd3aae23afa52361999cdf10b7
Ajay <ajaynayak617@gmail.com>
2024-06-22 09:26:33 +0530

    covid19

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,88 +1,58 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import BarChart from './BarChart';
 import PieChart from './PieChart';
 import CountrySelector from './CountrySelector';
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      countries: [],
-      selectedCountry: 'World'
-    };
-    this.handleCountryChange = this.handleCountryChange.bind(this);
-  }
+const Dashboard = () => {
+  const [data, setData] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('World');
 
-  componentDidMount() {
-    this.fetchCountries();
-    this.fetchData(this.state.selectedCountry);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.selectedCountry !== this.state.selectedCountry) {
-      this.fetchData(this.state.selectedCountry);
-    }
-  }
-
-  fetchCountries() {
-    axios.get('https://disease.sh/v3/covid-19/countries')
-      .then(response => {
-        const countries = response.data.map(country => country.country);
-        this.setState({ countries });
-      })
-      .catch(error => {
-        console.error('Error fetching countries:', error);
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data.map((country) => country.country));
       });
-  }
 
-  fetchData(country) {
-    const url = country === 'World'
-      ? 'https://disease.sh/v3/covid-19/all'
+    fetchData(selectedCountry);
+  }, [selectedCountry]);
+
+  const fetchData = (country) => {
+    const url = country === 'World' 
+      ? 'https://disease.sh/v3/covid-19/all' 
       : `https://disease.sh/v3/covid-19/countries/${country}`;
-
-    axios.get(url)
-      .then(response => {
-        const data = response.data;
-        console.log('Fetched data:', data); // Debug log
-        this.setState({
-          data: {
-            labels: ['Cases', 'Deaths', 'Recovered'],
-            cases: [data.cases, data.todayCases],
-            deaths: [data.deaths, data.todayDeaths],
-            recovered: [data.recovered, data.todayRecovered],
-          }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData({
+          labels: ['Cases', 'Deaths', 'Recovered'],
+          cases: [data.cases, data.todayCases],
+          deaths: [data.deaths, data.todayDeaths],
+          recovered: [data.recovered, data.todayRecovered],
         });
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
       });
-  }
+  };
 
-  handleCountryChange(e) {
-    this.setState({ selectedCountry: e.target.value });
-  }
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
 
-  render() {
-    const { data, countries, selectedCountry } = this.state;
+  if (!data) return <div>Loading...</div>;
 
-    if (!data) return <div>Loading...</div>;
-
-    return (
-      <div>
-        <Header />
-        <CountrySelector 
-          countries={['World', ...countries]} 
-          selectedCountry={selectedCountry} 
-          onChange={this.handleCountryChange} 
-        />
-        <BarChart data={data} />
-        <PieChart data={data} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <CountrySelector 
+        countries={['World', ...countries]} 
+        selectedCountry={selectedCountry} 
+        onChange={handleCountryChange} 
+      />
+      <BarChart data={data} />
+      <PieChart data={data} />
+    </div>
+  );
+};
 
 export default Dashboard;
